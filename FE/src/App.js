ad8aff94cb18be11e7ae71ea470dd5002305cc0d
SPAMallday <pjw090@gmail.com>
2022-07-28 13:05:48 +0900

    feat: temp save

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,8 +1,29 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Video from "./Components/Video/Video";
 
 function App() {
-  return <Button variant='contained'>Hello World</Button>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Link to='/'>
+          <Button>Main</Button>
+        </Link>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Link to='/video'>
+                <Button variant='contained'>Show Video</Button>
+              </Link>
+            }
+          ></Route>
+          <Route path='/video/*' element={<Video />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
