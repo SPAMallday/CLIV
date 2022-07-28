3d54bfbdb80b3297d9171be81dc86b7ba44046aa
최윤정 <chyuje96@naver.com>
2022-07-28 11:41:59 +0900

    chore: Modify navbar

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
-import "./App.css";
-import NavBar from "./NavBar";
+import './App.css';
+import NavBar from './component/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <NavBar />
     </div>
   );
