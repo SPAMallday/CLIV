fd0c6224ff10e8a3da064920e0d2eaf5c0d7558f
Jiwon <pjw090@gmail.com>
2022-08-02 19:46:40 +0900

    feat: Supplement VideoClass

--- FE/src/index.js
+++ FE/src/index.js
@@ -7,9 +7,9 @@ import registerServiceWorker from "./registerServiceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Root />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
