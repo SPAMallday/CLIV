30ecbbbb3b91914299acf435426335496f696356
hyejin <hyejin23@gmail.com>
2022-08-02 15:34:12 +0900

    chore: login BE add

--- BE/src/main/java/com/ssafy/crafts/CraftsApplication.java
+++ BE/src/main/java/com/ssafy/crafts/CraftsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CraftsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CraftsApplication.class, args);
 	}
 
