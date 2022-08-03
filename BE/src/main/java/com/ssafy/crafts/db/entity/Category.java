e8a6bd3ee2813255dddf93e17c0b07434f660e8c
허성은 <aysel0230@gmail.com>
2022-08-03 10:46:26 +0900

    Fix: update ClassInfo.java

--- BE/src/main/java/com/ssafy/crafts/db/entity/Category.java
+++ BE/src/main/java/com/ssafy/crafts/db/entity/Category.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Table(name = "TABLE")
@@ -15,4 +17,14 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @OneToMany(mappedBy = "category")
+    private List<ClassInfo> classes = new ArrayList<>();
+
+    public void addClass(ClassInfo classInfo) {
+        this.classes.add(classInfo);
+        if (classInfo.getCategory() != this) {
+            classInfo.setCategory(this);
+        }
+    }
 }
--- BE/src/main/java/com/ssafy/crafts/db/entity/ClassInfo.java
+++ BE/src/main/java/com/ssafy/crafts/db/entity/ClassInfo.java
@@ -19,19 +19,33 @@ public class ClassInfo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "class_id")
     private int id;
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "classInfo")
+    @OneToMany(mappedBy = "classInfo")
     private List<QnA> qnaList = new ArrayList<>();
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "classInfo")
+    @OneToMany(mappedBy = "classInfo")
     private List<Tagging> tagList = new ArrayList<>();
 
-    @OneToMany
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
-    private Auth teacher;
+
+    @OneToOne
+    @JoinColumn(name = "teacher_id")
+    private Member teacher;
+
+    @ManyToMany
+    @JoinTable(name = "CLASS_MEMBER",
+            joinColumns = @JoinColumn(name = "class_id"),
+            inverseJoinColumns = @JoinColumn(name = "member_id_array"))
     private List<Member> members = new ArrayList<Member>();
+
+    @Column(name = "class_name", nullable = false, length = 30)
     private String className;
+
     @Column(name = "class_datetime", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Timestamp classDatetime;
+
     private int durationH;
+
     private int headcount;
     private int price;
     private String guide;
@@ -45,6 +59,13 @@ public class ClassInfo {
 
     public void setQnaList(List<QnA> qnaList) {
         this.qnaList = qnaList;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+        if (!category.getClasses().contains(this)) {
+            category.getClasses().add(this);
+        }
     }
 
     public static enum ClassStatus {
