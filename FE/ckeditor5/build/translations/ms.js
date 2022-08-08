b9ab1a5aa3fdf3fec8f1c8dab042a8a093d69f91
Jiwon <pjw4037@naver.com>
2022-08-08 18:00:31 +0900

    feat: ckeditor add (need fix)

---
+++ FE/ckeditor5/build/translations/ms.js
@@ -0,0 +1,1 @@
+!function(n){const o=n.ms=n.ms||{};o.dictionary=Object.assign(o.dictionary||{},{"%0 of %1":"",Cancel:"","Remove color":"",Save:"","Show more items":"","Upload in progress":"Muat naik sedang berlangsung"}),o.getPluralForm=function(n){return 0}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
