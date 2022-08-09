42372587dd5aa428b2dc1fcc30117e47e38518b4
minju0919 <minju0919@naver.com>
2022-08-09 15:00:50 +0900

    feat: Add JPA Entity

--- BE/src/main/java/com/ssafy/crafts/db/entity/MBoardTeacher.java
+++ BE/src/main/java/com/ssafy/crafts/db/entity/MBoardTeacher.java
@@ -21,6 +21,9 @@ public class MBoardTeacher {
     @Column(name = "mt_id")
     private int id;
 
+    @Column(columnDefinition = "TINYINT", nullable = false, length = 1)
+    private int agreeYn;    // 클래스 개설 수강생 동의 여부
+
     // 1:1 관계 : 매칭_선생님 - 회원
     // MBoardTeacher 테이블의 teacher_id(PFK)를 사용해서 Member 테이블과 Join을 수행하고
     // 그 결과로 나온 값은 1건이며 이 데이터를 MBoardTeacher Entity의 Member에 매핑한다.
