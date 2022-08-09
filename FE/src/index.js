a370cf95ebeef1165a7d28a45358f4eef4beb9bd
hyejin <hyejin23@gmail.com>
2022-08-09 12:56:30 +0900

    chore:

--- FE/src/index.js
+++ FE/src/index.js
@@ -6,11 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import registerServiceWorker from './registerServiceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-);
+root.render(<Root />);
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
