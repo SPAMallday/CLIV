4b97737742e817c1757f8af24da2826f4d8777e7
허성은 <aysel0230@gmail.com>
2022-08-14 15:44:53 +0900

    chore:

--- BE/src/main/java/com/ssafy/crafts/db/repository/querydslRepo/ClassInfoQuerydslRepository.java
+++ BE/src/main/java/com/ssafy/crafts/db/repository/querydslRepo/ClassInfoQuerydslRepository.java
@@ -36,14 +36,6 @@ public class ClassInfoQuerydslRepository {
                 .where(qClassInfo.id.eq(id)).fetchOne();
         return classInfo;
     }
-
-    @Transactional
-    public void insertSessionId(String sessionId, int classId){
-        jpaQueryFactory.update(qClassInfo)
-                .set(qClassInfo.sessionId, sessionId)
-                .where(qClassInfo.id.eq(classId))
-                .execute();
-    }
     public List<ClassInfo> findClassInfoAll() {
         /**
          * @Method Name : findClassInfoAll
@@ -75,14 +67,14 @@ public class ClassInfoQuerydslRepository {
         /**
          * @Method Name : findClassInfoByClassTime
          * @작성자 : 허성은
-         * @Method 설명 : 수업 마감 시간이 임박한 수업 4개(변경 가능) 조회
+         * @Method 설명 : 수업 마감 시간이 임박한 수업 5개(변경 가능) 조회
          */
         return jpaQueryFactory
                 .select(qClassInfo)
                 .from(qClassInfo)
                 .where(qClassInfo.classStatus.eq(ClassInfo.ClassStatus.EXPECTED))
                 .orderBy(qClassInfo.classDatetime.desc())
-                .limit(4)
+                .limit(5)
                 .fetch();
     }
 
@@ -90,14 +82,14 @@ public class ClassInfoQuerydslRepository {
         /**
          * @Method Name : findClassInfoByHeadcount
          * @작성자 : 허성은
-         * @Method 설명 : 인원 마감이 임박한 수업 4개(변경 가능) 조회
+         * @Method 설명 : 인원 마감이 임박한 수업 5개(변경 가능) 조회
          */
         return jpaQueryFactory
                 .select(qClassInfo)
                 .from(qClassInfo)
                 .where(qClassInfo.classStatus.eq(ClassInfo.ClassStatus.EXPECTED))
                 .orderBy(qClassInfo.headcount.subtract(qClassInfo.members.size()).asc())
-                .limit(4)
+                .limit(5)
                 .fetch();
     }
 
@@ -112,7 +104,7 @@ public class ClassInfoQuerydslRepository {
                 .from(qClassInfo)
                 .leftJoin(qClassInfo.members, qMember)
                 .where(qClassInfo.members.contains(qMember)
-                        .and(qClassInfo.classStatus.eq(ClassInfo.ClassStatus.EXPECTED)))
+                    .and(qClassInfo.classStatus.eq(ClassInfo.ClassStatus.EXPECTED)))
                 .orderBy(qClassInfo.classDatetime.desc())
                 .fetch();
     }
@@ -144,18 +136,5 @@ public class ClassInfoQuerydslRepository {
                 .where(qClassInfo.classStatus.eq(ClassInfo.ClassStatus.EXPECTED))
                 .orderBy(qClassInfo.regdate.desc())
                 .fetch();
-    }
-
-    public List<Member> findClassMemberId(int classId) {
-        /**
-         * @Method Name : findClassMemberId
-         * @작성자 : 허성은
-         * @Method 설명 : 수업 신청을 한 학생들의 아이디를 반환.
-         */
-        return jpaQueryFactory
-                .select(qClassInfo.members)
-                .from(qClassInfo)
-                .where(qClassInfo.id.eq(classId))
-                .fetchOne();
     }
 }
